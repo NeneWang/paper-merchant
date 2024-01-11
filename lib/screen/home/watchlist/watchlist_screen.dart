f15ed8243360811337bfeee3b26ecf58d0136c44
NelsonWang <wangnelson2@gmail.com>
2024-01-11 00:10:31 -0500

    refactoring and cleaning application ::

--- lib/screen/home/watchlist/watchlist_screen.dart
+++ lib/screen/home/watchlist/watchlist_screen.dart
@@ -12,6 +12,7 @@ import 'package:papermarket/utils/color.dart';
 import 'package:papermarket/data/database.dart';
 import 'package:papermarket/data/utils.dart';
 import '../notification_screen.dart';
+import 'package:papermarket/components/loading_placeholder.dart';
 // Get Reload icon
 
 class WatchListScreen extends StatefulWidget {
@@ -28,6 +29,7 @@ class _WatchListScreenState extends State<WatchListScreen> {
   final Database db = Database();
   List stockList = [];
   List bookmarkedList = [];
+  bool loaded = false;
 
   reloadScreen({onlyDB = false, onlySync = false}) async {
     if (!onlySync) {
@@ -36,6 +38,9 @@ class _WatchListScreenState extends State<WatchListScreen> {
 
     if (!onlyDB) {
       await db.syncData();
+      setState(() {
+        loaded = true;
+      });
     }
 
     setState(() {
@@ -55,6 +60,184 @@ class _WatchListScreenState extends State<WatchListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var userHero = [
+      Align(
+        alignment: Alignment.topRight,
+        child: Padding(
+          padding: EdgeInsets.only(
+              right: Get.width / 20.57,
+              top: Get.height / 178.28,
+              bottom: Get.height / 178.28),
+          child: InkWell(
+            onTap: () {
+              reloadScreen();
+            },
+            child: const Icon(
+              CupertinoIcons.refresh,
+              size: 24, // Adjust the size as needed
+            ),
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.topRight,
+        child: Padding(
+          padding: EdgeInsets.only(
+              right: Get.width / 20.57, top: Get.height / 178.28),
+          child: InkWell(
+            onTap: () {
+              Get.to(NotificationScreen());
+            },
+            child: const Icon(
+              CupertinoIcons.bell,
+              size: 24, // Adjust the size as needed
+            ),
+          ),
+        ),
+      ),
+      Container(
+        height: Get.height / 5,
+        child: TabBarView(
+          controller: myTabController.controller,
+          children: [
+            tabAssetsView(db.userData["cash"]?.toStringAsFixed(2) ?? "0",
+                db.userData["papel_asset_worth"]?.toStringAsFixed(2) ?? "0"),
+            Text(Get.height.toString()),
+            Text(Get.width.toString()),
+          ],
+        ),
+      ),
+    ];
+
+    var StocksData = [
+      Container(
+        height: Get.height * 1.2 /*(Get.height > 891) ? Get.height : 275*/,
+        width: Get.width,
+        decoration: const BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  right: Get.width / 31.64,
+                  top: Get.height / 89.14,
+                  left: Get.width / 31.64),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Stocks",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: black0D1F3C,
+                      fontFamily: "NunitoBold",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AllStockScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "See All",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: appColor,
+                        fontFamily: "NunitoSemiBold",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListView.builder(
+              padding: EdgeInsets.only(top: 10),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: stockList.length,
+              itemBuilder: (context, index) => simpleStockListViewItem(
+                context: context,
+                title: stockList[index]["title"],
+                total: stockList[index]["totalRs"],
+                imageUrl: stockList[index]["imageUrl"],
+                stockData: db.userStockPrices[stockList[index]["title"]],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 14),
+              child: Divider(
+                height: 2,
+                thickness: 2,
+                color: grayF2F2F2,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  right: Get.width / 31.64, left: Get.width / 31.64),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Bookmark",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: black0D1F3C,
+                      fontFamily: "NunitoBold",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      /*  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => AllStockScreen(),
+                                    ),
+                                  );*/
+                    },
+                    child: const Text(
+                      "See All",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: appColor,
+                        fontFamily: "NunitoSemiBold",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListView.builder(
+              padding: EdgeInsets.only(top: 0),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: bookmarkedList.length,
+              itemBuilder: (context, index) => simpleStockListViewItem(
+                context: context,
+                title: bookmarkedList[index]["title"],
+                total: bookmarkedList[index]["totalRs"],
+                imageUrl: bookmarkedList[index]["imageUrl"],
+                stockData:
+                    db.userBookmarkPrices[bookmarkedList[index]["title"]],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ];
     return Scaffold(
       body: RefreshIndicator(
         onRefresh: () async {
@@ -65,183 +248,17 @@ class _WatchListScreenState extends State<WatchListScreen> {
           child: Column(
             children: [
               appBarDesign(),
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      right: Get.width / 20.57,
-                      top: Get.height / 178.28,
-                      bottom: Get.height / 178.28),
-                  child: InkWell(
-                    onTap: () {
-                      reloadScreen();
-                    },
-                    child: const Icon(
-                      CupertinoIcons.refresh,
-                      size: 24, // Adjust the size as needed
-                    ),
-                  ),
-                ),
+              Column(
+                children: userHero,
               ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      right: Get.width / 20.57, top: Get.height / 178.28),
-                  child: InkWell(
-                    onTap: () {
-                      Get.to(NotificationScreen());
-                    },
-                    child: const Icon(
-                      CupertinoIcons.bell,
-                      size: 24, // Adjust the size as needed
-                    ),
-                  ),
+              if (!loaded)
+                LoadingPlaceholder(
+                  waitingMessage: "Loading your data...",
                 ),
-              ),
-              Container(
-                height: Get.height / 5,
-                child: TabBarView(
-                  controller: myTabController.controller,
-                  children: [
-                    tabAssetsView(
-                        db.userData["cash"]?.toStringAsFixed(2) ?? "0",
-                        db.userData["papel_asset_worth"]?.toStringAsFixed(2) ??
-                            "0"),
-                    Text(Get.height.toString()),
-                    Text(Get.width.toString()),
-                  ],
+              if (loaded)
+                Column(
+                  children: StocksData,
                 ),
-              ),
-              Container(
-                height:
-                    Get.height * 1.2 /*(Get.height > 891) ? Get.height : 275*/,
-                width: Get.width,
-                decoration: const BoxDecoration(
-                  color: white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                          right: Get.width / 31.64,
-                          top: Get.height / 89.14,
-                          left: Get.width / 31.64),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "Stocks",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: black0D1F3C,
-                              fontFamily: "NunitoBold",
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AllStockScreen(),
-                                ),
-                              );
-                            },
-                            child: const Text(
-                              "See All",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: appColor,
-                                fontFamily: "NunitoSemiBold",
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    ListView.builder(
-                      padding: EdgeInsets.only(top: 10),
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: stockList.length,
-                      itemBuilder: (context, index) => simpleStockListViewItem(
-                        context: context,
-                        title: stockList[index]["title"],
-                        total: stockList[index]["totalRs"],
-                        imageUrl: stockList[index]["imageUrl"],
-                        stockData:
-                            db.userStockPrices[stockList[index]["title"]],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 14),
-                      child: Divider(
-                        height: 2,
-                        thickness: 2,
-                        color: grayF2F2F2,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          right: Get.width / 31.64, left: Get.width / 31.64),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "Bookmark",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: black0D1F3C,
-                              fontFamily: "NunitoBold",
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              /*  Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AllStockScreen(),
-                                ),
-                              );*/
-                            },
-                            child: const Text(
-                              "See All",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: appColor,
-                                fontFamily: "NunitoSemiBold",
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    ListView.builder(
-                      padding: EdgeInsets.only(top: 0),
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: bookmarkedList.length,
-                      itemBuilder: (context, index) => simpleStockListViewItem(
-                        context: context,
-                        title: bookmarkedList[index]["title"],
-                        total: bookmarkedList[index]["totalRs"],
-                        imageUrl: bookmarkedList[index]["imageUrl"],
-                        stockData: db
-                            .userBookmarkPrices[bookmarkedList[index]["title"]],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
             ],
           ),
         ),
